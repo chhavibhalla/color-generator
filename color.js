8bb66d50dd158a37120ee81b26ac30733cc7384d
chhavibhalla <bhallachhavi007@gmail.com>
2025-02-06 19:07:52 +0530

    yess

---
+++ color.js
@@ -0,0 +1,11 @@
+function color(){
+    const randomnum = Math.random();
+    const num =  Math.floor(randomnum*16777215);
+    const randnum = "#"+ num.toString(16);
+
+    document.body.style.backgroundColor=randnum;
+   
+    
+}
+
+document.getElementById("button").addEventListener("click", color);
